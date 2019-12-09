eb7c41624769ab8c4e2ff104b1f82313e3298756
Kiran Kumar Bijjala <kirankumar.bijjala@sjsu.edu>
2019-12-08 23:45:52 -0800

    Changes

--- Frontend/src/components/Main.js
+++ Frontend/src/components/Main.js
@@ -6,22 +6,22 @@ import Ownerlogin from './Ownerlogin';
 import OwnersignUp from './Ownersignup';
 import NavBar from './NavBar';
 import Buyerprofile from './Buyerprofile';
-import Search from './Search';
-import Ownerprofile from './Ownerprofile';
-import OwnerDetails from './OwnerDetails/OwnerDetails';
-import Menu from './Menu/Menu';
-import Orders from './Orders/Orders';
-import RestaurantMenu from './RestaurantMenu/RestaurantMenu';
-import PastOrders from './PastOrders/PastOrders';
-import RestaurantOrders from './RestaurantOrders/RestaurantOrders';
-import RestaurantPastOrders from './RestaurantPastOrders/RestaurantPastOrders';
-import UpdateMenu from './UpdateMenu/UpdateMenu';
-import OwnerRestaurantMenu from './OwnerRestaurantMenu/OwnerRestaurantMenu';
+// import Search from './Search';
+// import Ownerprofile from './Ownerprofile';
+// import OwnerDetails from './OwnerDetails/OwnerDetails';
+// import Menu from './Menu/Menu';
+// import Orders from './Orders/Orders';
+// import RestaurantMenu from './RestaurantMenu/RestaurantMenu';
+// import PastOrders from './PastOrders/PastOrders';
+// import RestaurantOrders from './RestaurantOrders/RestaurantOrders';
+// import RestaurantPastOrders from './RestaurantPastOrders/RestaurantPastOrders';
+// import UpdateMenu from './UpdateMenu/UpdateMenu';
+// import OwnerRestaurantMenu from './OwnerRestaurantMenu/OwnerRestaurantMenu';
 import Home from './Home/Home';
-import OwnerMessage from "./OwnerMessage";
-import BuyerMessage from "./BuyerMessage";
-import Reply from "./Reply";
-import Askquestion from "./Askquestion";
+// import OwnerMessage from "./OwnerMessage";
+// import BuyerMessage from "./BuyerMessage";
+// import Reply from "./Reply";
+// import Askquestion from "./Askquestion";
 
 //Create a Main Component
 class Main extends Component {
@@ -35,17 +35,18 @@ class Main extends Component {
                 <Route path="/home" component={Home}/>
                 <Route path="/ownerlogin" component={Ownerlogin}/>
                 <Route path="/ownersignup" component={OwnersignUp}/>
-                 {/* <Route path="/search" component={Search}/>  */}
                 <Route path="/userprofile" component={Buyerprofile}/>
+                 {/* <Route path="/search" component={Search}/>  */}
+                {/* 
                 <Route path="/ownerprofile" component={Ownerprofile}/>
                 <Route path="/signup" component={Buyersignup}/> 
                 <Route path="/ownerlogin" component={Ownerlogin}/>
                 <Route path="/ownersignup" component={OwnersignUp}/>
                 <Route path="/ownerprofile" component={Ownerprofile}/>
-                <Route path="/ownerdetails" component={OwnerDetails}/>
-                <Route path="/menu" component={Menu}/>
+                <Route path="/ownerdetails" component={OwnerDetails}/> */}
+                {/* <Route path="/menu" component={Menu}/> */}
                 <Route path="/home" component={Home}/>
-                <Route path="/restaurantmenu" component={RestaurantMenu}/>
+                {/* <Route path="/restaurantmenu" component={RestaurantMenu}/>
                 <Route path="/userorder" component={Orders}/>
                 <Route path="/userpastorder" component={PastOrders}/>
                 <Route path="/restaurantorders" component={RestaurantOrders}/>
@@ -55,7 +56,7 @@ class Main extends Component {
                 <Route path="/OwnerMessage" component ={OwnerMessage} exact/>  
                 <Route path="/BuyerMessage" component ={BuyerMessage} exact/>  
                 <Route path="/Reply" component ={Reply} exact/>  
-                <Route path="/Askquestion" component ={Askquestion} exact/> 
+                <Route path="/Askquestion" component ={Askquestion} exact/>  */}
             </div>
         )
     }
